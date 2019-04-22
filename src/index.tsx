e43f36d0cccb0a8bb0c281d1f1059e2bbafc9774
Thành Trang <trangcongthanh91@gmail.com>
2019-04-22 17:12:13 +0700

    Init library

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled('div')`
+  background: red;
+`
+
+type Props = {}
+
+export const Test: React.FC<Props> = () => (
+  <Container>AB alksdjflaksjdflkjsalkfjskl</Container>
+)
---
+++ stories/button.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Test } from '../src';
+
+storiesOf('Button', module)
+  .add('ahihi', () => <Test>what?</Test>);   
